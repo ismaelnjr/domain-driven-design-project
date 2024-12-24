af5888b23266c404000578b1c8d6825c527151bc
ismaelnjr <ismaelnjr@icloud.com>
2024-12-24 07:52:47 -0300

    v.0.0.1

--- application/services/car_service.py
+++ application/services/car_service.py
@@ -6,28 +6,28 @@ class CarService:
     def __init__(self, car_repository: ModelRepositoryInterface):
         self.car_repository = car_repository
 
-    def create_car(self, car: Car):
+    def save(self, car: Car):
         car.id = self.car_repository.add_car(car)
         return car
 
-    def get_car_by_id(self, car_id: int) -> Car:
+    def get_by_id(self, car_id: int) -> Car:
         car = self.car_repository.get_car_by_id(car_id)
         if not car:
             raise ValueError(f"Carro com ID {car_id} não encontrado.")
         return car
 
-    def list_cars(self) -> list[Car]:
+    def list_all(self) -> list[Car]:
         cars = self.car_repository.get_all_cars()
         return [car for car in cars]
 
-    def update_car(self, car_id: int, updated_car: Car):
+    def update(self, car_id: int, updated_car: Car):
         car = self.car_repository.get_car_by_id(car_id)
         if not car:
             raise ValueError(f"Carro com ID {car_id} não encontrado.")
         updated_car.id = car_id
         self.car_repository.update_car(updated_car)
 
-    def delete_car(self, car_id: int):
+    def delete(self, car_id: int):
         car = self.car_repository.get_car_by_id(car_id)
         if not car:
             raise ValueError(f"Carro com ID {car_id} não encontrado.")
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from database.orm.models import init_db, get_session
 from repositories.sqlite_model_repository import SQLiteModelRepository
 from application.services.car_service import CarService
 from application.services.car_model_service import CarModelService
@@ -11,17 +10,17 @@ if __name__ == "__main__":
     # Instanciando o repositório e o serviço
     repositorio = SQLiteModelRepository("carros.db")
     car_service = CarService(repositorio)
-    car_model_service = CarModelService(repositorio)
+    model_service = CarModelService(repositorio)
 
     # Criando alguns modelos e carros
-    car_model1 = CarModel(name="Opala", manufacturer="Chevrolet", year=1970)    
-    car_model_service.create_car_model(car_model1)
+    model1 = CarModel(name="Opala", manufacturer="Chevrolet", year=1970)    
+    model_service.save(model1)
 
-    car1 = Car(plate="ABC-1234", model_id=car_model1.id, specification=CarSpecification(color="Preto", fuel_type="Gasolina", horsepower=100, transmission="Manual"))
-    car_service.create_car(car1)
+    car1 = Car(plate="ABC-1234", model_id=model1.id, specification=CarSpecification(color="Preto", fuel_type="Gasolina", horsepower=100, transmission="Manual"))
+    car_service.save(car1)
 
     # Listando os carros criados
-    print(car_service.list_cars())
+    print(car_service.list_all())
 
     # Listando os modelos de carros criados
-    print(car_model_service.list_car_models())
+    print(model_service.list_all())
